504df25d4c5fc08a1dbfa0b78008ad78f7bbf88e
Gerald-006 <jacksongeraldobere@gmail.com>
2023-07-31 00:03:40 +0300

    navigation bar added

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,62 @@
 <template>
-  <div>
-  <router-link to="/">Home</router-link> |
-   <router-link to="/projects">Projects</router-link>
-   <router-link to="/about">About</router-link>
-   <router-link to="/blog">Blog</router-link>
-  </div>
+  <header class="flex items-center bg-white h-12 border-b-2">
+    <nav class="flex justify-center flex-1">
+      <div class="space-x-16 text-base">
+        <router-link
+          to="/"
+          class="p-3 hover:bg-blue-300 hover:rounded"
+          active-class="active-link"
+          exact-active-class="active-link"
+          >Home</router-link
+        >
+        <router-link
+          to="/projects"
+          class="p-3 hover:bg-blue-300 hover:rounded active:border-b-4 active:border-blue-500"
+          active-class="active-link"
+          exact-active-class="active-link"
+          >Projects</router-link
+        >
+        <router-link
+          to="/about"
+          class="p-3 hover:bg-blue-300 hover:rounded"
+          active-class="active-link"
+          exact-active-class="active-link"
+          >About</router-link
+        >
+        <router-link
+          to="/blog"
+          class="p-3 hover:bg-blue-300 hover:rounded"
+          active-class="active-link"
+          exact-active-class="active-link"
+          >Blog</router-link
+        >
+      </div>
+    </nav>
+
+    <div @click="toggleMode" class="flex justify-end pr-20">
+      <i v-if="lightMode" class="far fa-lightbulb text-lg"></i>
+      <i v-if="darkMode" class="fas fa-lightbulb text-lg"></i>
+    </div>
+  </header>
+
   <router-view />
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
-    
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      lightMode: true,
+      darkMode: false,
+    };
+  },
+  methods: {
+    toggleMode() {
+      this.lightMode = !this.lightMode;
+      this.darkMode = !this.darkMode;
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,6 @@
 <template>
-    <div>hello</div>
+    <div>hello
+        
+    </div>
+    
 </template>
