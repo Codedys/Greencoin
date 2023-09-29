fe737ffc00495a070a88f47ce4bab0f1998daef8
Gerald-006 <jacksongeraldobere@gmail.com>
2023-09-29 15:44:44 +0300

    small twitch

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,11 +1,7 @@
 <template>
      <div>
     <div class="h-32 w-72 mx-auto mt-8">
-      <h6 class="font-bold text-center text-3xl"><strong>Projects</strong></h6>
-
-      <p class="text-center text-xl mt-2 mb-2 ">
-        A Fusion of Stories, Projects,Tech and Cars.
-      </p>
+      <h6 class="font-bold text-center text-3xl"><strong>Blogs</strong></h6>
       <div  class="border-t-8 w-16 h-3 mx-auto rounded border-blue-600"></div>
     </div>
   </div>
